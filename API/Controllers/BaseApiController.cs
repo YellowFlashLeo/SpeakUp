d2a2c5e129b32a15f586ec758220f1cbff900482
Leonid <Leonid.Lobanov@derivco.ee>
2021-07-06 11:15:28 +0300

    Finished CRUD in Core

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,6 +8,9 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private  IMediator _mediator;
+        // if private instance is null we assign mediator from services by suing hhtpContext
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
     }
 }
