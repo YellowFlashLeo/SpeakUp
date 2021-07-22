e3e7710c9f904a56a27d32e3b16ac4474b5846f2
Leonid <Leonid.Lobanov@derivco.ee>
2021-07-22 14:24:57 +0300

    Basaic Routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,23 @@
-import { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBaR';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponents';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 // useState in React Hook which allows to store state inside Component
 // [activities] will name of variable where state will be stored
 // [setActivities] function to state the state
 // useState([]), here we delcare initial state for activities
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]) // will ensure it only runs once, otherwise we will call api and get data, setActivities will assign data to activities and since React component chnages. It causes rerender of the entire component,
-  // so useEffect will be triggered again and again (endless loop)
-
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/activities' component={ActivityDashboard} />
+        <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
+import LoadingComponent from '../../../app/layout/LoadingComponents';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
@@ -12,9 +13,17 @@ import ActivityList from './ActivityList';
 
 // {activities[0] && means do whats on the right if left is not null or undefined
 export default observer(function ActivityDashboard() {
-
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]) // will ensure it only runs once, otherwise we will call api and get data, setActivities will assign data to activities and since React component chnages. It causes rerender of the entire component,
+    // so useEffect will be triggered again and again (endless loop)
+
+    if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
+
+
     return (
         <Grid>
             <Grid.Column width='10'>
