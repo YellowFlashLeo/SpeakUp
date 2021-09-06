42b9df709e41bdfddc3e46aec12283f1937cfcfc
Leonid <Leonid.Lobanov@derivco.ee>
2021-09-06 12:55:02 +0300

    Using interceptor to get error resposne from Api

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+import { textSpanContainsTextSpan } from 'typescript';
 import { IActivity } from '../modules/activity';
 
 
@@ -12,13 +14,26 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 // says to do smthing when we getting response back from API 
 axios.interceptors.response.use(async response => {
-    try {
-        await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+
+    await sleep(1000);
+    return response;
+}, (error: AxiosError) => {
+    const { data, status } = error.response!;
+    switch (status) {
+        case 400:
+            toast.error('bad request');
+            break;
+        case 401:
+            toast.error('unauthorised');
+            break;
+        case 404:
+            toast.error('not found');
+            break;
+        case 500:
+            toast.error('server error');
+            break;
     }
+    return Promise.reject(error);
 })
 // makes life easier, by getting response and storing to responseBody object
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
@@ -34,8 +49,8 @@ const Activities = {
     list: () => requests.get<IActivity[]>('/activities'),
     details: (id: string) => requests.get<IActivity>(`/activities/${id}`),
     create: (activity: IActivity) => axios.post('/activities', activity),
-    update: (activity:IActivity) => axios.put(`/activities/${activity.id}`,activity),
-    delete: (id:string) => axios.delete(`/activities/${id}`)
+    update: (activity: IActivity) => axios.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete(`/activities/${id}`)
 }
 
 const agent = {
