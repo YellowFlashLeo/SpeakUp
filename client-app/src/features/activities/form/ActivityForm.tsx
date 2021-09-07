ff501528aceec3b8dbcbdcfd5c538425bb125769
Leonid <Leonid.Lobanov@derivco.ee>
2021-09-07 15:44:32 +0300

    Inital Validation in Formik

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,11 +3,12 @@ import { ChangeEvent } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, FormField, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik,Form,Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { values } from 'mobx';
 
 
@@ -32,6 +33,9 @@ export default observer(function ActivityForm() {
         venue: ''
     });
 
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The activity title is required')
+    })
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!)) //! means we know what we are doing(activity can be undefined)
     }, [id, loadActivity]);
@@ -58,22 +62,29 @@ export default observer(function ActivityForm() {
    */
 
 
-      // Formik 
-      // IntialValues are populated from useState
-      // after useEffect we need to update values, so we use enableReinitialize 
-      // 
+    // Formik 
+    // IntialValues are populated from useState
+    // after useEffect we need to update values, so we use enableReinitialize 
+    // 
     if (loadingInitial) return <LoadingComponent content='Loading activity...' />
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <Field placeholder='Title' name='title'/>
-                        <Field placeholder='Description' name='description'/>
-                        <Field placeholder='Category' name='category'/>
-                        <Field type='date' placeholder='Date' name='date'/>
-                        <Field placeholder='City' name='city'/>
-                        <Field placeholder='Venue' name='venue'/>
+                        <FormField>
+                            <Field placeholder='Title' name='title' />
+                            <ErrorMessage name='title' render={error => <Label basic color='red' content={error} />} />
+                        </FormField>
+                        <Field placeholder='Description' name='description' />
+                        <Field placeholder='Category' name='category' />
+                        <Field type='date' placeholder='Date' name='date' />
+                        <Field placeholder='City' name='city' />
+                        <Field placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
                     </Form>
