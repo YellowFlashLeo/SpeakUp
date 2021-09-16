588924a87459a69ca71d682793449568ce46a9df
Leonid <Leonid.Lobanov@derivco.ee>
2021-09-16 12:25:04 +0300

    Dispkaying errors in the form

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,21 +1,29 @@
-import { Form, Formik } from "formik";
+
+import { ErrorMessage, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { useStore } from "../../app/stores/store";
 
 export default observer(function LoginForm() {
     const { userStore } = useStore();
+    // In case we get 401 from our Api we will use SetError method from formik in the catch block
+    // and display invalid parameters message inside errorMessage component and then in label
     return (
         <Formik
-            initialValues={{ email: '', password: '' }}
-            onSubmit={values => userStore.login(values)}
+            initialValues={{ email: '', password: '', error: null }}
+            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error =>
+                setErrors({ error: 'Invalid parameters' }))}
         >
-            {({ handleSubmit, isSubmitting }) => (
+            {({ handleSubmit, isSubmitting, errors }) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                     <MyTextInput name='email' placeholder='Email' />
                     <MyTextInput name='password' placeholder='Password' type='password' />
+                    <ErrorMessage
+                        name='error' render={() =>
+                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
+                    />
                     <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
                 </Form>
             )}
