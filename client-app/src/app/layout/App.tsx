2d033f5ee8b64006863b8ad6ef695c6b867763a3
Leonid <Leonid.Lobanov@derivco.ee>
2021-09-16 17:07:05 +0300

    Persisting login

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -11,6 +11,9 @@ import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/activities/errors/NotFound';
 import ServerError from '../../features/activities/errors/ServerError';
 import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponents';
 
 // useState in React Hook which allows to store state inside Component
 // [activities] will name of variable where state will be stored
@@ -18,6 +21,19 @@ import LoginForm from '../../features/users/LoginForm';
 // useState([]), here we delcare initial state for activities
 function App() {
   const location = useLocation(); // this will make sure that component is rerendered if path props change
+  const { commonStore, userStore } = useStore();
+
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
+  
   return (
     <>
       <ToastContainer position='bottom-right' hideProgressBar />
@@ -33,8 +49,8 @@ function App() {
                 <Route path='/activities/:id' component={ActivityDetails} />
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
                 <Route path='/errors' component={TestErrors} />
-                <Route path='/server-error' component={ServerError}/>
-                <Route path='/login' component={LoginForm}/>
+                <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,12 +1,26 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../modules/serverError";
 
 export default class CommonStore {
     error: ServerError | null = null;
-    token: string | null = null;
+    token: string | null = window.localStorage.getItem('jwt');
     appLoaded = false;
     constructor() {
         makeAutoObservable(this);
+
+        // first param is what we are reacting to
+        // what we do as reaction
+        // reaction doest run when store is initially loads but it only runs when there is change in token 
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    window.localStorage.setItem('jwt', token)
+                } else {
+                    window.localStorage.removeItem('jwt')
+                }
+            }
+        )
     }
 
     setServerError = (error: ServerError) => {
@@ -14,7 +28,6 @@ export default class CommonStore {
     }
 
     setToken = (token: string | null) => {
-        if (token) window.localStorage.setItem('jwt', token);
         this.token = token;
     }
 
