dd54e27cdcef455836e191a5a540d337417d7e40
Leonid <Leonid.Lobanov@derivco.ee>
2021-09-21 15:48:08 +0300

    JoinTable for Many-to-Many

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,9 +6,33 @@ namespace Persistence
 {
     public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext(DbContextOptions options) : base(options) 
+        public DataContext(DbContextOptions options) : base(options)
         {
         }
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Composite Primary Key of ActivityAttendee table
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+ 
+            // Many-to-Many relationship
+            builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.AppUser)
+            .WithMany(a => a.Activities)
+            .HasForeignKey(aa => aa.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(a => a.Activity)
+            .WithMany(u => u.Attendees)
+            .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
+
+
+// In order, to create new migration by using EFCORE in VScode
+// dotnet ef migrations add ActivityAttendee -p Persistence -s API  (ActivityAttendee is simply name of migration)
